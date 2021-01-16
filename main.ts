696e0300fc89a75f72d64df718882e27de632cd7
Liang Yu Chen <andymememe@users.noreply.github.com>
2021-01-16 11:46:54 +0800

    Add: Sound

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
     Started = 1
     Disalarm = 0
-    Timer = 0
+    Timer = 5
     while (true) {
+        music.playTone(659, music.beat(BeatFraction.Quarter))
         basic.showNumber(Timer)
-        if (Timer == 5) {
+        if (Timer == 0) {
+            music.stopMelody(MelodyStopOptions.All)
+            music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.OnceInBackground)
             basic.showIcon(IconNames.Skull)
             break;
         }
         if (Disalarm == 1) {
+            music.stopMelody(MelodyStopOptions.All)
+            music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
             basic.showIcon(IconNames.Happy)
             break;
         }
-        basic.pause(1000)
-        Timer += 1
+        basic.pause(800)
+        Timer += -1
     }
     Started = 0
 })
